e851e69a97de0c48479da32bdfdf5231c629154b
VanHelsing <119530744+VanHes1ng@users.noreply.github.com>
2023-09-10 21:33:11 +0300

    Update SP500_Cycles.py

--- SP500_Cycles.py
+++ SP500_Cycles.py
@@ -129,10 +129,5 @@ st.set_page_config(
     page_title="Ex-stream-ly Cool App",
     page_icon="🦈",
     layout="wide",
-    initial_sidebar_state="expanded",
-    menu_items={
-        'Get Help': 'https://www.extremelycoolapp.com/help',
-        'Report a bug': "https://www.extremelycoolapp.com/bug",
-        'About': "# This is a header. This is an *extremely* cool app!"
-    }
+    initial_sidebar_state="expanded"
 )
