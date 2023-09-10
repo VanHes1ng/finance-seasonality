9e6d15e21dffe46d5c824fa894f235306a110037
VanHelsing <119530744+VanHes1ng@users.noreply.github.com>
2023-09-10 20:21:57 +0300

    Update SP500_Cycles.py

--- SP500_Cycles.py
+++ SP500_Cycles.py
@@ -9,12 +9,17 @@ import matplotlib.pyplot as plt
 import monthly_returns_heatmap as mrh
 import calendar
 
+# Set the title for the Streamlit app
 st.subheader("S&P500 Cycles")
 
+# Create a sidebar for user settings
 st.sidebar.header("Settings")
 
 # User Inputs
-start_date = st.sidebar.date_input("Start Date", pd.to_datetime('1990-01-01'),min_value=pd.to_datetime('1960-01-01'), max_value=pd.to_datetime('2035-01-01'))
+# Allow the user to select a start date within the specified range
+start_date = st.sidebar.date_input("Start Date", pd.to_datetime('1990-01-01'), min_value=pd.to_datetime('1960-01-01'), max_value=pd.to_datetime('2035-01-01'))
+
+# Allow the user to select an end date
 end_date = st.sidebar.date_input("End Date", pd.to_datetime('2024-01-01'))
 
 # Download S&P 500 data from Yahoo Finance
@@ -27,13 +32,35 @@ day_returns = data["Close"].pct_change()
 # Calculate ROC (Rate of Change)
 data["ROC"] = ((data["Close"] - data["Close"].shift(16)) / data["Close"].shift(16)) * 100
 
-def plot(x,y):
-    y_column_name = y.name #if hasattr(y, 'name') else 'Unknown'
+
+# Z-Score
+# Calculate 15-day Simple Moving Average (SMA)
+data["SMA_15"] = data["Close"].rolling(window=15).mean()
+# Calculate 15-day Rolling Standard Deviation
+data["STD_15"] = data["Close"].rolling(window=15).std()
+# Calculate Z scores
+data["Z_15"] = (data["Close"] - data["SMA_15"])/data["STD_15"]
+
+
+# Define a function to plot data using Plotly
+def plot(x, y):
+    # Get the name of the y column
+    y_column_name = y.name
+    
+    # Create a Plotly figure for the data
     data_fig = go.Figure(data=[go.Scatter(x=x, y=y)])
+    
+    # Set the title of the chart using the y column name
     data_fig.update_layout(title=f"{y_column_name} chart")
+    
+    # Display the chart in the Streamlit app
     st.plotly_chart(data_fig)
- 
+
+# Plot the closing price data
 plot(data.index, data["Close"])
 
+# Plot the ROC data
 plot(data.index, data["ROC"])
 
+# Plot the Z data
+plot(data.index, data["Z_15"])
