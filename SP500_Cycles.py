370348724a72ea18bea283f825f1cbda6d7b4382
VanHelsing <119530744+VanHes1ng@users.noreply.github.com>
2023-09-11 18:02:16 +0300

    Update SP500_Cycles.py

--- SP500_Cycles.py
+++ SP500_Cycles.py
@@ -20,7 +20,7 @@ st.set_page_config(
 )
 
 st.header(':green[SPY] Economic Cycles', divider='rainbow')
-st.subheader(':gray[s&p500]')
+st.subheader(':gray[Special Edition]')
 
 image = Image.open('pngegg.png')
 st.sidebar.image(image)
