af2d39c04d70f495de4feb7d0a74eeadeca96be1
VanHelsing <119530744+VanHes1ng@users.noreply.github.com>
2023-09-11 19:21:18 +0300

    Update SP500_Cycles.py

--- SP500_Cycles.py
+++ SP500_Cycles.py
@@ -168,7 +168,7 @@ st.image(image)
 
 st.write(
     "About",
-    " S&P Cycles is not a financial advisor", 
-    " Made with Streamlit v1.26.0 https://streamlit.io",
-    "Copyright 2023 Snowflake Inc. All rights reserved."
+    "\n S&P Cycles is not a financial advisor", 
+    "\nMade with Streamlit v1.26.0 https://streamlit.io",
+    "\nCopyright 2023 Snowflake Inc. All rights reserved."
 )
