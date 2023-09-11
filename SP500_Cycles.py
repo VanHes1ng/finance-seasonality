ec472bf29649ffca512f4d7a6e006e50e3c37e1d
VanHelsing <119530744+VanHes1ng@users.noreply.github.com>
2023-09-11 17:33:14 +0300

    Update SP500_Cycles.py

--- SP500_Cycles.py
+++ SP500_Cycles.py
@@ -134,8 +134,6 @@ with col1:
 with col2:
    plot(data.index, data["Sortino Ratio"], title="Sortino Ratio", line_color='orange', line_style='solid', width = 700, height = 600)
 
-plot(data.index, data["ROC"], title="Rate of Change", line_color='green', line_style='solid')
-plot(data.index, data["Z Score"], title="Z Score", line_color='purple', line_style='solid')
-#plot(data.index, data["Sharpe Ratio"], title="Sharpe Ratio", line_style='solid')
-#plot(data.index, data["Sortino Ratio"], title="Sortino Ratio", line_color='orange', line_style='solid')
-plot(data.index, data["MACD"], title="MACD", line_color='blue', is_histogram=True)
+plot(data.index, data["ROC"], title="Rate of Change", line_color='green', line_style='solid', height = 800)
+plot(data.index, data["Z Score"], title="Z Score", line_color='purple', line_style='solid', height = 800)
+plot(data.index, data["MACD"], title="MACD", line_color='blue', is_histogram=True, height = 800)
