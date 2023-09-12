c964d8361196adc485540c0193f6ca2582c800d3
VanHelsing <119530744+VanHes1ng@users.noreply.github.com>
2023-09-12 20:12:26 +0300

    Update SP500_Cycles.py

--- SP500_Cycles.py
+++ SP500_Cycles.py
@@ -174,16 +174,17 @@ st.write(
 )
 
 chart_data = pd.DataFrame(
-    data["Close"],
-    columns=['Close'])
+    data,
+    columns=['Sortino Ratio'])
 
 chart_data1 = pd.DataFrame(
     data,
-    columns=['AVG', 'AVG_6']
+    columns=['Sortino Ratio']
     )
 
 col1, col2 = st.columns(2)
 with col1:
     st.line_chart(chart_data)
+
 with col2:
-    st.line_chart(chart_data1+chart_data, color=["#0e734f", "#b3100b"])
+    st.line_chart(chart_data1)
