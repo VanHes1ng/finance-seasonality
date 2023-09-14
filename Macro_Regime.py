a559d44c1f16597dc59a9dd57e781055c46abd05
VanHelsing <119530744+VanHes1ng@users.noreply.github.com>
2023-09-14 19:41:18 +0300

    Update Macro_Regime.py

--- Macro_Regime.py
+++ Macro_Regime.py
@@ -18,7 +18,7 @@ st.set_page_config(
 )
 
 # Initialize Fred API
-fred = Fred(api_key="YOUR_API_KEY")  # Replace with your actual API key
+fred = Fred(api_key="20256d454ab4cfe9d4a9672dff8337b0")  # Replace with your actual API key
 
 def get_data(ticker):
     start = '2022-01-31'
