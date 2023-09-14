ecc323d3f76aedeeb8a33c9dd528606a97ba1f0c
VanHelsing <119530744+VanHes1ng@users.noreply.github.com>
2023-09-14 21:06:35 +0300

    Update Macro_Regime.py

--- Macro_Regime.py
+++ Macro_Regime.py
@@ -51,7 +51,7 @@ indicator_list = ["CAPUTLG3311A2S",
     "NFCI",
     "BAMLH0A0HYM2EY",
     "T10Y2Y",
-    "USINTR"]
+    "FEDFUNDS"]
 
 data = pd.DataFrame()  # Create a dictionary to store data for each indicator
 
