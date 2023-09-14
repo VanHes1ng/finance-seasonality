e05f146af88451bc40886cbe70402792db428a36
VanHelsing <119530744+VanHes1ng@users.noreply.github.com>
2023-09-14 20:38:16 +0300

    Update Macro_Regime.py

--- Macro_Regime.py
+++ Macro_Regime.py
@@ -22,7 +22,7 @@ st.set_page_config(
 fred = Fred(api_key="20256d454ab4cfe9d4a9672dff8337b0")  # Replace with your actual API key
 
 def get_data(ticker):
-    start = '1960-01-31'
+    start = '1990-01-31'
     end = '2025-01-01'
     data = pd.DataFrame(fred.get_series(
         ticker,
