b05481be6e232a76d165ee672173dd97449068e1
VanHelsing <119530744+VanHes1ng@users.noreply.github.com>
2023-09-15 19:32:19 +0300

    Update Macro_Regime.py

--- Macro_Regime.py
+++ Macro_Regime.py
@@ -93,9 +93,9 @@ def roc(src, len, smooth):
 
 
 
-data["ROC"] = roc(data["AVG"], 15, 5)
+data["ROC"] = roc(data["AVG"], 10, 5)
 
-data["ROC1"] = data["ROC"].shift(1)
+data["ROC1"] = data["ROC"] - data["ROC"].shift(10)
 
 plot(data.index, data["ROC"], data["ROC1"], "ROC")
 
