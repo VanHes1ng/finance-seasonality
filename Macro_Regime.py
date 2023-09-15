fb63202a6d3189039a6a4292d10e01d943d42728
VanHelsing <119530744+VanHes1ng@users.noreply.github.com>
2023-09-15 20:02:49 +0300

    Update Macro_Regime.py

--- Macro_Regime.py
+++ Macro_Regime.py
@@ -77,13 +77,13 @@ def plot(x, y1, y2, title, y1_name='Primary Y-Axis', y2_name='Secondary Y-Axis',
     fig.add_trace(go.Scatter(x=x, y=y1, mode='lines', name=y1_name, line=dict(color=y1_color)))
     
     fig.update_layout(
-        yaxis=dict(title=y1_name, titlefont=dict(color=y1_color), showgrid=False, zeroline=True, height = 200),
-        yaxis2=dict(title=y2_name, titlefont=dict(color=y2_color), overlaying='y', side='right', showgrid=False, zeroline=True, height = 200)
+        yaxis=dict(title=y1_name, titlefont=dict(color=y1_color), showgrid=False, zeroline=True),
+        yaxis2=dict(title=y2_name, titlefont=dict(color=y2_color), overlaying='y', side='right', showgrid=False, zeroline=True)
     )
     
     fig.add_trace(go.Scatter(x=x, y=y2, mode='lines', name=y2_name, line=dict(color=y2_color), yaxis='y2'))
     
-    fig.update_layout(title=title)
+    fig.update_layout(title=title, height=400)
     st.plotly_chart(fig, use_container_width=True)
 
 
