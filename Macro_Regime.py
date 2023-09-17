55a6e039d8998be516d89b58926cdd7487390611
VanHelsing <119530744+VanHes1ng@users.noreply.github.com>
2023-09-17 21:59:27 +0300

    Update Macro_Regime.py

--- Macro_Regime.py
+++ Macro_Regime.py
@@ -170,7 +170,7 @@ fig.add_shape(
 )
 
 # Add a point for the average values with red color
-avg_trace = go.Scatter(x=avg_data['Last_X'], y=avg_data['Last_Y'], text='avg', mode='markers', marker=dict(color='red'))
+avg_trace = go.Scatter(x=avg_data['Change(4)'], y=avg_data['Rate of Change'], text='avg', mode='markers', marker=dict(color='red'))
 fig.add_trace(avg_trace)
 
 avg_trace1 = go.Scatter(x=data["last_Roc1"], y=data["last_Roc"], text='avg', mode='markers', marker=dict(color='orange'))
