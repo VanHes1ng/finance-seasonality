728ac4d638bc9a118e8534eaa4a6b0aa3e3e561d
VanHelsing <119530744+VanHes1ng@users.noreply.github.com>
2023-09-17 13:30:42 +0300

    Update Macro_Regime.py

--- Macro_Regime.py
+++ Macro_Regime.py
@@ -125,7 +125,7 @@ grid_data = {'x': last_x, 'y': last_y}
 grid_data = pd.DataFrame(grid_data)
 
 # Create a scatter plot using Plotly Express for the grid
-fig = px.scatter(grid_data, x='x', y='y', title='Grid with Zero at the Center')
+fig = px.scatter(grid_data, x=last_x, y=last_y, title='Grid with Zero at the Center')
 
 # Customize the layout
 fig.update_xaxes(range=[-100, 100])
