0ce7156a5a3dd12f5e7fb49d2587d4a4628d0c24
VanHelsing <119530744+VanHes1ng@users.noreply.github.com>
2023-09-17 10:54:19 +0300

    Update SPY_Prediction.py

--- SPY_Prediction.py
+++ SPY_Prediction.py
@@ -15,12 +15,24 @@ def fetch_stock_data(symbol, start_date, end_date):
 
 # Function to preprocess data and create features
 def preprocess_data(df):
+    # Create a column for predicting price increases
     df['Price_Up'] = df['Close'] > df['Close'].shift(-1)
     df['Price_Up'] = df['Price_Up'].astype(int)
+    
+    # Create a column for predicting price decreases
+    df['Price_Down'] = df['Close'] < df['Close'].shift(-1)
+    df['Price_Down'] = df['Price_Down'].astype(int)
+    
     return df
 
-# Function to build and train a logistic regression model
-def build_logistic_regression_model(X_train, y_train):
+# Function to build and train a logistic regression model for price increase
+def build_logistic_regression_model_up(X_train, y_train):
+    model = LogisticRegression()
+    model.fit(X_train, y_train)
+    return model
+
+# Function to build and train a logistic regression model for price decrease
+def build_logistic_regression_model_down(X_train, y_train):
     model = LogisticRegression()
     model.fit(X_train, y_train)
     return model
@@ -40,51 +52,73 @@ def main():
     # Preprocess data
     df = preprocess_data(df)
     
-    # Select features and target
+    # Select features and targets
     X = df[['Open', 'High', 'Low', 'Volume']]
-    y = df['Price_Up']
+    y_up = df['Price_Up']
+    y_down = df['Price_Down']
     
-    # Split data into training and testing sets
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    # Split data into training and testing sets for price increase prediction
+    X_train_up, X_test_up, y_train_up, y_test_up = train_test_split(X, y_up, test_size=0.2, random_state=42)
     
-    # Build and train the logistic regression model
-    model = build_logistic_regression_model(X_train, y_train)
+    # Split data into training and testing sets for price decrease prediction
+    X_train_down, X_test_down, y_train_down, y_test_down = train_test_split(X, y_down, test_size=0.2, random_state=42)
     
-    # Model evaluation
-    y_pred = model.predict(X_test)
-    accuracy = accuracy_score(y_test, y_pred)
+    # Build and train the logistic regression models
+    model_up = build_logistic_regression_model_up(X_train_up, y_train_up)
+    model_down = build_logistic_regression_model_down(X_train_down, y_train_down)
     
-    st.write("## Model Evaluation")
-    st.write(f"Accuracy: {accuracy:.2f}")
+    # Model evaluation for price increase prediction
+    y_pred_up = model_up.predict(X_test_up)
+    accuracy_up = accuracy_score(y_test_up, y_pred_up)
     
-    st.write("## Classification Report")
-    st.write(classification_report(y_test, y_pred))
+    st.write("## Model Evaluation for Price Increase Prediction")
+    st.write(f"Accuracy: {accuracy_up:.2f}")
     
-    st.write("## Confusion Matrix")
-    cm = confusion_matrix(y_test, y_pred)
+    st.write("## Classification Report for Price Increase Prediction")
+    st.write(classification_report(y_test_up, y_pred_up))
     
-    # Visualize confusion matrix as a Plotly heatmap
-    st.write("### Confusion Matrix Heatmap")
-    fig_cm = ff.create_annotated_heatmap(
-        z=cm,
+    # Confusion matrix for price increase prediction
+    cm_up = confusion_matrix(y_test_up, y_pred_up)
+    
+    # Visualize confusion matrix as a Plotly heatmap for price increase prediction
+    st.write("### Confusion Matrix Heatmap for Price Increase Prediction")
+    fig_cm_up = ff.create_annotated_heatmap(
+        z=cm_up,
         x=['Predicted 0', 'Predicted 1'],
         y=['Actual 0', 'Actual 1'],
         colorscale='Blues'
     )
-    fig_cm.update_layout(xaxis_title='Predicted', yaxis_title='Actual')
-    st.plotly_chart(fig_cm)
+    fig_cm_up.update_layout(xaxis_title='Predicted', yaxis_title='Actual')
+    st.plotly_chart(fig_cm_up)
+    
+    # Model evaluation for price decrease prediction
+    y_pred_down = model_down.predict(X_test_down)
+    accuracy_down = accuracy_score(y_test_down, y_pred_down)
+    
+    st.write("## Model Evaluation for Price Decrease Prediction")
+    st.write(f"Accuracy: {accuracy_down:.2f}")
+    
+    st.write("## Classification Report for Price Decrease Prediction")
+    st.write(classification_report(y_test_down, y_pred_down))
+    
+    # Confusion matrix for price decrease prediction
+    cm_down = confusion_matrix(y_test_down, y_pred_down)
+    
+    # Visualize confusion matrix as a Plotly heatmap for price decrease prediction
+    st.write("### Confusion Matrix Heatmap for Price Decrease Prediction")
+    fig_cm_down = ff.create_annotated_heatmap(
+        z=cm_down,
+        x=['Predicted 0', 'Predicted 1'],
+        y=['Actual 0', 'Actual 1'],
+        colorscale='Blues'
+    )
+    fig_cm_down.update_layout(xaxis_title='Predicted', yaxis_title='Actual')
+    st.plotly_chart(fig_cm_down)
     
     # Visualize stock price data
     st.write("## SPY Stock Price Data")
     fig_stock_price = px.line(df, x=df.index, y='Close', title='SPY Stock Price')
     st.plotly_chart(fig_stock_price)
-    
-    # Visualize the logistic regression model's predictions
-    st.write("## Model Predictions vs. Actual")
-    df['Predicted_Up'] = model.predict(X)
-    
-    fig_predictions = px.line(df, x=df.index, y=['Price_Up', 'Predicted_Up'], title='Model Predictions vs. Actual')
-    st.plotly_chart(fig_predictions)
     
 if __name__ == "__main__":
     main()
