95ee36067cb380c7f140193e5f10f6a4fbd7800b
VanHelsing <119530744+VanHes1ng@users.noreply.github.com>
2023-09-17 14:05:58 +0300

    Update Macro_Regime.py

--- Macro_Regime.py
+++ Macro_Regime.py
@@ -119,7 +119,7 @@ for i in indicator_list:
 # Create a DataFrame with the grid data
 grid_data = {'x': x_values, 'y': y_values}
 
-grid_data = pd.DataFrame(grid_data.tail(),index=indicator_list)
+grid_data = pd.DataFrame(grid_data,index=indicator_list)
 
 st.write(grid_data)
 
