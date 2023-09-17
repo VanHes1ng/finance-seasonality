fb2679f98afe00a5e02cd96a9a5af488b9d60b35
VanHelsing <119530744+VanHes1ng@users.noreply.github.com>
2023-09-17 14:10:13 +0300

    Update Macro_Regime.py

--- Macro_Regime.py
+++ Macro_Regime.py
@@ -115,6 +115,10 @@ grid_data = pd.DataFrame(index=indicator_list)
 for i in indicator_list:
     y_values = roc(data[i], 4, 5)
     x_values = y_values - y_values.shift(4)
+    # Update grid_data1 for each indicator
+    grid_data.loc[i, 'x'] = x_values.iloc[-1]  # Get the last value of x_values
+    grid_data.loc[i, 'y'] = y_values.iloc[-1]  # Get the last value of y_values
+
 
 # Create a DataFrame with the grid data
 grid_data = {'x': x_values, 'y': y_values}
@@ -124,7 +128,7 @@ grid_data1 = pd.DataFrame(grid_data,index=indicator_list)
 st.write(grid_data1)
 
 # Create a scatter plot using Plotly Express for the grid
-fig = px.scatter(grid_data, x='x', y='y', title='Grid with Zero at the Center')
+fig = px.scatter(grid_data1, x='x', y='y', title='Grid with Zero at the Center')
 
 # Customize the layout
 fig.update_xaxes(range=[-100, 100])
