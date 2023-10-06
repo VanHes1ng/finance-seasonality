8a2b8dce25b4bc451853b9f7895a69461929f192
VanHelsing <119530744+VanHes1ng@users.noreply.github.com>
2023-10-06 20:29:01 +0300

    Update Z.py

--- Z.py
+++ Z.py
@@ -36,8 +36,8 @@ data["VIX"] = vix["Close"]
 data["Z"] = z_score(data["VIX"], 20)
 
 st.subheader("SPY")
-st.line_chart(data, y = "SPY", color="blue",height = 600)
+st.line_chart(data, y = "SPY", color="#26afd1",height = 600)
 st.subheader("VIX")
-st.line_chart(data, y = "VIX", color="orange", height = 600)
+st.line_chart(data, y = "VIX", color="#d1a626", height = 600)
 
-st.line_chart(data, y = "Z", color="green")
+st.line_chart(data, y = "Z", color="#26d128")
