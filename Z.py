111a588d737a78726de3874d786b3a1c35fa110d
VanHelsing <119530744+VanHes1ng@users.noreply.github.com>
2023-10-07 19:43:51 +0300

    Update Z.py

--- Z.py
+++ Z.py
@@ -52,7 +52,7 @@ def space(number):
         st.write("#")
 
 
-_,  col_2,  _  = st.columns([1, 3, 1])
+_,  col_2,  _  = st.columns([0.5, 3, 0.5])
 col_2.header("SPY Volatility Direction Index", divider="blue")
 
 col1,  col2,  col3, _  = st.columns([1, 4, 3, 0.5])
