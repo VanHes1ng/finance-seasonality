7c8d9bca0492d157d541a7c4799325b1f2ae5b56
VanHelsing <119530744+VanHes1ng@users.noreply.github.com>
2023-10-07 19:20:38 +0300

    Update Z.py

--- Z.py
+++ Z.py
@@ -85,7 +85,7 @@ data["Z"] = z_score(data["VIX"], 20)
 
 with col2:
     st.header("SPY Volatility Direction Index", divider="blue")
-    space(2)
+    space(1)
     st.subheader("SPY")
     st.line_chart(data, y = "SPY", color="#26afd1",height = 300, use_container_width=True)
 
