f8261c4971dbf43f183df363ed93dce1991aa5cb
VanHelsing <119530744+VanHes1ng@users.noreply.github.com>
2023-10-07 18:50:01 +0300

    Update Z.py

--- Z.py
+++ Z.py
@@ -93,7 +93,8 @@ with col22:
     st.subheader("VIX")
     st.line_chart(data, y = "VIX", color= "#d1a626", height = 300, use_container_width=True)
     st.line_chart(data, y = "Z", color="#26d128", height = 250, use_container_width=True)
-    st.metric(label="Z - Score of VIX", value=data["Z"].iloc(-1))
+    st.metric(label="Z-Score", value=data["Z"].iloc(-1), delta=-0.5,
+    delta_color="inverse")
 
 with col33:
     st.markdown("***")
