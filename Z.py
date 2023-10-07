c254ccc80eb32ef88a0658ce03792b8837bb250f
VanHelsing <119530744+VanHes1ng@users.noreply.github.com>
2023-10-07 18:37:14 +0300

    Update Z.py

--- Z.py
+++ Z.py
@@ -41,8 +41,7 @@ col11, col22, col33, _ = st.columns([1, 4, 3, 0.5])
 
 # Inputs
 with col3:
-    st.text("")
-    st.text("")
+    st.markdown('##')
     st.markdown("""
             This system is designed to provide valuable insights into future market movements,
             enabling users to make informed decisions regarding their investments without directly executing trades. 
@@ -70,7 +69,6 @@ data["Z"] = z_score(data["VIX"], 20)
 
 with col2:
     st.title("SPY volatility direction")
-    st.markdown("***")
     st.subheader("SPY")
     st.line_chart(data, y = "SPY", color="#26afd1",height = 300, use_container_width=True)
 
