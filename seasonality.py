d2e5687af67e3ee80d6d169819f2f143b6e60d55
VanHelsing <119530744+VanHes1ng@users.noreply.github.com>
2023-10-13 22:29:12 +0300

    Update seasonality.py

--- seasonality.py
+++ seasonality.py
@@ -106,7 +106,7 @@ percentage_changes_fig.add_trace(go.Bar(y=heatmap_data.columns,
                         x=np.round(monthly_percentage_changes,2), 
                         orientation='h', 
                         marker=go.bar.Marker(color=np.cos(x/3),
-                                             colorscale="Bluered_r",
+                                             colorscale="RdBu",
                                              colorbar=dict(title="value"),
                                              line=dict(color="rgb(0, 0, 0)",width=1))
                         )
