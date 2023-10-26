771fa89067d35cdd3307d64bfed112aa2d90d12f
VanHelsing <119530744+VanHes1ng@users.noreply.github.com>
2023-10-26 20:17:25 +0300

    Update Z.py

--- Z.py
+++ Z.py
@@ -102,7 +102,7 @@ data["Z"] = z_score(data["VIX"], 20)
 
 space(1)
 col2.subheader("SPY")
-col2.line_chart(data, y = "SPY", color="#26afd1",height = 450, use_container_width=True)
+col2.line_chart(data, y = "SPY", color="#26afd1",height = 500, use_container_width=True)
 
 with col22:
     st.markdown("***")
