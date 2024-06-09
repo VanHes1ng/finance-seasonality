8a4217c69680ff5ebd891f4bc8da708c6fd1abcf
VanHelsing <ivan.kochergat@gmail.com>
2024-06-09 16:09:45 +0300

    Create 2_About.py

---
+++ pages/2_About.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+#-------------------------------------------------------------------------------------------------------
+# Page Configurations
+#-------------------------------------------------------------------------------------------------------
+
+st.set_page_config(page_title = "About", layout="wide", initial_sidebar_state="expanded", page_icon= "@")
+
+# Titles
+st.header("About Seasonality", divider="gray")
+
+st.sidebar.header("About")
